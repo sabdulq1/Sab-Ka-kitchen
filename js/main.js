4802263d64fda7890e8db6fafc6d2c95b5ad1da8
sabdulq1 <105384724+sabdulq1@users.noreply.github.com>
2022-06-08 04:56:51 -0400

    d

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+function myFunction(){
+    var m =document.getElementById("menu")
+    if(m.style.display=="block"){
+        m.style.display="none";
+    }
+    else{
+        m.style.display="block";
+    }
+}
